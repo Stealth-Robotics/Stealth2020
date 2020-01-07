d09a5e3254e7dc76b522eb61898478fe65dd6285
4089Software <stealthrobotics@gmail.com>
2020-01-06 18:36:48 -0800

    Created project

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap 
+{
+  
+    //ports for drivebase talons
+    public static final int driveLF = 8;
+    public static final int driveLR = 2;
+    public static final int driveRF = 7;
+    public static final int driveRR = 1;
+
+    /*
+    PDP0 = 
+    */
+}
---
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -0,0 +1,18 @@
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveBase extends SubsystemBase {
+  /**
+   * Creates a new DriveBase.
+   */
+  public DriveBase() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
