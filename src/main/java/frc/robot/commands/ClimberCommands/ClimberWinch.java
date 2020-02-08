a3227f2a2b1391d376db73413c10e42d553d8852
4089Software <stealthrobotics@gmail.com>
2020-02-07 19:30:53 -0800

    Create ClimberWinch.java
    Added ClimberWinch command to move the winch for the climber.

---
+++ src/main/java/frc/robot/commands/ClimberCommands/ClimberWinch.java
@@ -0,0 +1,51 @@
+
+package frc.robot.commands.ClimberCommands;
+
+import frc.robot.subsystems.PanelControl;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+/**
+ * This command rotates the control panel to the desired location
+ */
+public class ClimberWinch extends CommandBase 
+{
+    
+    /**
+     * Creates a new ExampleCommand.
+     *
+     * @param subsystem The subsystem used by this command.
+     */
+    public ClimberWinch(PanelControl panelControl) 
+    {
+       
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() 
+    {
+       
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() 
+    {
+        
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) 
+    {
+        
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() 
+    {
+        return false;
+    }
+}
