86162e306e654a092638281fdeb590cac5e974f0
SamarthRao91 <raosamarth91@gmail.com>
2020-02-14 14:41:40 -0800

    Added comments to robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -135,7 +135,7 @@ public class RobotContainer
         new JoystickButton(mechJoystick, 3).whenHeld(new StartEndCommand(
             () -> climber.setWinchSpeed(mechJoystick.getMagnitude()),
              () -> climber.setWinchSpeed(mechJoystick.getMagnitude()), climber));
-
+          //Move elevator motrs down when the winch goes up
              new JoystickButton(mechJoystick, 3).whenHeld(new StartEndCommand(
                 () -> climber.setClimbElevatorSpeed(-mechJoystick.getMagnitude()),
                  () -> climber.setClimbElevatorSpeed(-mechJoystick.getMagnitude()), climber));
