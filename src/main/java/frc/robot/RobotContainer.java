2a4c14be7d941be2f91979ceb45a9e42ac63e27d
SamarthRao91 <raosamarth91@gmail.com>
2020-02-19 14:46:51 -0800

    Added comments to robot container for climber

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -117,7 +117,7 @@ public class RobotContainer
         new JoystickButton(driveJoystick, 1).whenPressed(() -> driveBase.reverseDrive());
 
         new JoystickButton(driveJoystick, 2).whenPressed(new AlignWithTarget(driveBase, limelight));  
-        new JoystickButton(mechJoystick , 4 /*neds to be changed to the buttons on the driver station*/).whenPressed(() -> Climber.runClimb());
+       // new JoystickButton(mechJoystick , 4 /*neds to be changed to the buttons on the driver station*/).whenPressed(() -> Climber.runClimb());
     }
   
   
