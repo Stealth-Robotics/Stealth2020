223084b37a93750f3eb8d5914cf80869a2878d44
Siddharth Rao <siddrao.926@gmail.com>
2020-02-26 22:09:56 -0800

    Removed Unused Imports

--- src/main/java/frc/robot/commands/EncoderAutoCommands/SixBallAutoCommandGroup.java
+++ src/main/java/frc/robot/commands/EncoderAutoCommands/SixBallAutoCommandGroup.java
@@ -1,18 +1,15 @@
 package frc.robot.commands.EncoderAutoCommands;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DrivebaseCommands.AlignWithTarget;
-import frc.robot.commands.EncoderAutoCommands.DriveBackwards;
-import frc.robot.commands.EncoderAutoCommands.DriveForward;
-import frc.robot.commands.EncoderAutoCommands.TurnToAngle;
-import frc.robot.commands.IntakeCommands.IntakeFuel;
 import frc.robot.commands.ShooterCommands.AimHood;
 import frc.robot.commands.ShooterCommands.FireShooter;
 import frc.robot.subsystems.Belts;
-import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.DistanceSensor;
+import frc.robot.subsystems.DriveBase;
+import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Limelight;
-import frc.robot.subsystems.*;
+import frc.robot.subsystems.Shooter;
 public class SixBallAutoCommandGroup extends SequentialCommandGroup {
 
   /**
