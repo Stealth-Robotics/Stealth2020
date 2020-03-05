59be03c0d19f240199c60c3c28762a7a95533236
4089Software <stealthrobotics@gmail.com>
2020-03-04 19:54:32 -0800

    Constant Changes

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -94,7 +94,18 @@ public final class Constants
     public static final class AutoConstants 
     {
         /*public static final double kMaxSpeedMetersPerSecond = 3;
-        public static final double kMaxAcceleration; //TODO: Tune drivebase PID on carpet
+        public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+
+        // Reasonable baseline values for a RAMSETE follower in units of meters and seconds
+        public static final double kRamseteB = 2;
+        public static final double kRamseteZeta = 0.7;*/
+
+        private static final double tickPerMotorRevolution = 360;
+        private static final double driveGearReduction = 1;
+        private static final double wheeldiameterInches = 6.00;
+        public static final double ticksPerInch = (tickPerMotorRevolution * driveGearReduction) / (wheeldiameterInches * Math.PI);
+
+        public static final double basekP = 0.02; //TODO: Tune drivebase PID on carpet
         public static final double basekI = 0;
         public static final double basekD = 0;
 
